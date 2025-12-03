ed116ce6115bb3a5781ad2d69969073f0a9c89fa
cringey303 <lucas@the-b.org>
2025-12-03 00:12:32 -0600

    finished add_note() valid input loop and changed some visuals

--- main.rs
+++ main.rs
@@ -24,6 +24,7 @@ struct Note {
 }
 
 fn print_menu() {
+    println!("-----------------");
     println!("1. Add Note");
     println!("2. Remove Note");
     println!("3. View Notes");
@@ -32,16 +33,31 @@ fn print_menu() {
 }
 
 fn add_note(notes: &mut Vec<Note>) -> io::Result<()> {
-    println!("Enter note: ");
-    let mut body = String::new();
-    io::stdin().read_line(&mut body)?;
+    let body: String = loop {
+        println!("Enter note ('q' to cancel): ");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+
+        //cancel
+        if input.trim().eq_ignore_ascii_case("q") {
+            println!(">Cancelled");
+            return Ok(());
+        }
+        //empty note
+        if input.trim().is_empty() {
+            println!("Error: Note cannot be empty");
+            continue; //restart loop
+        }
+
+        break input.trim().to_string();
+    };
 
     let now = Local::now();
     let now_formatted = now.format("%Y-%m-%d %H:%M:%S").to_string();
     
     let new_note = Note {
         id: notes.len() as u32 + 1,
-        body: body.trim().to_string(),
+        body: body,
         timestamp: now_formatted,
     };
 
@@ -50,7 +66,6 @@ fn add_note(notes: &mut Vec<Note>) -> io::Result<()> {
     Ok(())
 }
 
-//TODO: loop to keep taking integer until it is < len notes
 fn remove_note(notes: &mut Vec<Note>) -> io::Result<()> {
     loop {
         println!("Enter note ID to remove ('q' to cancel): ");
@@ -106,22 +121,25 @@ fn main() -> io::Result<()> {
             if let Err(e) = add_note(&mut notes) {
                 eprint!("Error adding note: {}", e);
             }
-            println!();
 
         //remove note
         } else if clean_input == 2 {
             if let Err(e) = remove_note(&mut notes) {
                 eprint!("Error removing note: {}", e);
             }
-            println!();
 
         //view notes
         } else if clean_input == 3 {
-            println!("------NOTES------");
-            for note in &notes {
-                println!("{}. {} ({})", note.id, note.body, note.timestamp);
+            if notes.is_empty() {
+                println!("No notes found");
+                continue;
+            } else { 
+                println!("------NOTES------");
+                for note in &notes {
+                    println!("{}. {} ({})", note.id, note.body, note.timestamp);
+                }
+                println!("-----------------\n");
             }
-            println!("-----------------\n");
 
         //quit
         } else if clean_input == 4 {
@@ -129,7 +147,6 @@ fn main() -> io::Result<()> {
             break; //quit
         } else {
             println!("Error: Invalid choice");
-            println!();
         }
     }
     Ok(())
